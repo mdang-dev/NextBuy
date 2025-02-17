461f25a25f809439a6e8f32c9093305d88f9f522
mdang-dev <minhdang25.dev@gmail.com>
2025-02-17 10:45:51 +0700

    add navbar (#2)

--- frontend/nextbuy/src/components/globals/Navbar.tsx
+++ frontend/nextbuy/src/components/globals/Navbar.tsx
@@ -13,7 +13,7 @@ const Navbar: React.FC = () => {
 
     return (
         <nav className="bg-white text-white shadow-2xl">
-            <div className="max-w-7xl mx-auto px-4 py-3 flex items-center justify-between">
+            <div className="w-full px-12 py-3 flex items-center justify-between">
                 <div className="flex items-center">
                     <Link href={'/'}>
                         <span className="text-2xl font-bold cursor-pointer text-black">NextBuy</span>
@@ -21,49 +21,51 @@ const Navbar: React.FC = () => {
                 </div>
 
 
-                <div className="hidden md:flex items-center px-2 bg-white overflow-hidden gap-2">
-                    <Input
-                        type="text"
-                        placeholder="Search products..."
-                        className="px-3 py-2 focus:border-none text-black rounded-2xl outline-none w-96"
-                    />
-                    <button className="bg-blue-500 w-28 h-10 flex rounded-3xl">
-                        <div className={`m-auto flex items-center gap-2`}>
-                            <Search size={20}/>
-                            Search
-                        </div>
-                    </button>
-                </div>
-
-                <ul className="hidden md:flex space-x-6 text-black">
-                    <li className="hover:text-gray-300 cursor-pointer">Home</li>
-                    <li className="hover:text-gray-300 cursor-pointer">Shop</li>
-                    <li className="hover:text-gray-300 cursor-pointer">Deals</li>
-                    <li className="hover:text-gray-300 cursor-pointer">Contact</li>
-                </ul>
-
-                <div className="flex items-center space-x-4">
-
-                    <div className={`cursor-pointer`}>
-                        <User2 size={28} color={'black'}/>
+                <div className={`flex items-center gap-4`}>
+                    <div className="flex items-center px-2 bg-white gap-2 max-sm:gap-0">
+                        <Input
+                            type="text"
+                            placeholder="Search products..."
+                            className="py-2 max-sm:hidden 2xl:w-[800px] lg:w-max focus:border-none text-black rounded-2xl outline-none"
+                        />
+                        <button className="bg-blue-500 w-28 h-10 flex rounded-3xl max-sm:w-10">
+                            <div className={`m-auto flex items-center gap-2`}>
+                                <Search size={20}/>
+                                <span className={`max-sm:hidden max-md:hidden max-lg:hidden`}>Search</span>
+                            </div>
+                        </button>
                     </div>
 
-                    <div className="relative cursor-pointer">
-                        <ShoppingCart size={28} color={'black'}/>
-                        <span
-                            className="absolute -top-2 -right-2 bg-red-600 text-white text-xs w-5 h-5 flex items-center justify-center rounded-full">
+                    <ul className="hidden md:flex space-x-6 text-black">
+                        <li className="hover:text-gray-300 cursor-pointer">Home</li>
+                        <li className="hover:text-gray-300 cursor-pointer">Shop</li>
+                        <li className="hover:text-gray-300 cursor-pointer">Deals</li>
+                        <li className="hover:text-gray-300 cursor-pointer">Contact</li>
+                    </ul>
+
+                    <div className="flex items-center space-x-4">
+
+                        <div className={`cursor-pointer`}>
+                            <User2 size={28} color={'black'}/>
+                        </div>
+
+                        <div className="relative cursor-pointer">
+                            <ShoppingCart size={28} color={'black'}/>
+                            <span
+                                className="absolute -top-2 -right-2 bg-red-600 text-white text-xs w-5 h-5 flex items-center justify-center rounded-full">
               3
             </span>
-                    </div>
+                        </div>
 
-                    <button className="md:hidden" onClick={toggleMenu}>
-                        {menuOpen ? <X size={28} color={'black'}/> : <Menu size={28} color={'black'}/>}
-                    </button>
+                        <button className="md:hidden" onClick={toggleMenu}>
+                            {menuOpen ? <X size={28} color={'black'}/> : <Menu size={28} color={'black'}/>}
+                        </button>
+                    </div>
                 </div>
             </div>
 
             {menuOpen && (
-                <ul className="md:hidden bg-white flex flex-col items-center py-4 space-y-4 text-black transition-all ease-in-out duration-200">
+                <ul className={`md:hidden bg-white flex flex-col items-center py-4 space-y-4 text-black ${menuOpen && 'transition-all ease-in-out duration-200'}`}>
                     <li className="hover:text-gray-300 cursor-pointer">Home</li>
                     <li className="hover:text-gray-300 cursor-pointer">Shop</li>
                     <li className="hover:text-gray-300 cursor-pointer">Deals</li>
